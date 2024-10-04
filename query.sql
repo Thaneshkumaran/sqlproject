468d1711ff91b00528cdc89bedeca61359fcf5c4
Thaneshkumaran <thaneshr42@gmail.com>
2024-10-04 11:35:49 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -36,8 +36,17 @@ order by order_year desc;
 
 --Show unique birth years from patients and order them by ascending.
 select distinct year(birth_date) from patients
-order by birth_date
+order by birth_date;
 
+--Show unique first names from the patients table which only occurs once in the list.
+--For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output
+select first_name from patients
+group by first_name
+having count(first_name)<=1;
 
-
-
+--Show patient_id, attending_doctor_id, and diagnosis for admissions that match one of the two criteria:
+--1. patient_id is an odd number and attending_doctor_id is either 1, 5, or 19.
+--2. attending_doctor_id contains a 2 and the length of patient_id is 3 characters.
+select patient_id,attending_doctor_id,diagnosis from admissions
+where (attending_doctor_id in(1,5,19) and patient_id%2!=0)
+ or (attending_doctor_id like "%2%" and len(patient_id)=3)
