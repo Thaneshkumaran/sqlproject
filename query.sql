e9dec2684760fbd533218293b0768f088e5c05ac
Thaneshkumaran <thaneshr42@gmail.com>
2024-10-04 11:33:18 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,43 @@
-use ho
+use hospital;
+-- 1.Show the employee's first_name and last_name, a "num_orders" column with a count of the orders taken, and a column called "Shipped" that displays "On Time" if the order shipped_date is less or equal to the required_date, "Late" if the order shipped late.
+-- Order by employee last_name, then by first_name, and then descending by number of orders
+SELECT
+  e.first_name,
+  e.last_name,
+  COUNT(o.order_id) As num_orders,
+  (
+    CASE
+      WHEN o.shipped_date <= o.required_date THEN 'On Time'
+      ELSE 'Late'
+    END
+  ) AS shipped
+FROM orders o
+  JOIN employees e ON e.employee_id = o.employee_id
+GROUP BY
+  e.first_name,
+  e.last_name,
+  shipped
+ORDER BY
+  e.last_name,
+  e.first_name,
+  num_orders DESC
+
+--Show how much money the company lost due to giving discounts each year, order the years from most recent to least recent. Round to 2 decimal places
+Select 
+YEAR(o.order_date) AS 'order_year' , 
+ROUND(SUM(p.unit_price * od.quantity * od.discount),2) AS 'discount_amount' 
+
+from orders o 
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+
+group by YEAR(o.order_date)
+order by order_year desc;
+
+--Show unique birth years from patients and order them by ascending.
+select distinct year(birth_date) from patients
+order by birth_date
+
+
+
+
